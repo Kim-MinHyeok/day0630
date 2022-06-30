069f181dd123292c2c4ee5c1442b1d9f4c69e672
Kim-MinHyeok <48408314+Kim-MinHyeok@users.noreply.github.com>
2022-06-30 09:46:22 +0900

    MemberController 완성

---
+++ day0630/src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "로그인";
+	}
+	
+	@RequestMapping("/loginOK")
+	public String loginOK() {
+		return "redirect:/listBoard";
+	}
+}
